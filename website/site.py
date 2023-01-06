b8bfd1ea65c79db8342bf69f9081417821e674a4
Redtronics <development@redtronics.de>
2023-01-06 23:28:57 +0100

    add flask handler

---
+++ website/site.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+
+class WebserverNyria(Flask):
+    def __init__(self):
+        super().__init__(__name__)
+
+    def site(self):
+        @self.route("/")
+        def index():
+            return render_template("index.html")
+
+        self.run()
