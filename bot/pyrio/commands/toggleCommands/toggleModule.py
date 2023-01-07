fe7f83e32812c177e6e12d6fa95d6ba3de5a65d7
Redtronics <development@redtronics.de>
2023-01-07 13:34:52 +0100

    add toggle module template

---
+++ bot/pyrio/commands/toggleCommands/toggleModule.py
@@ -0,0 +1,19 @@
+import nextcord
+from nextcord.ext import commands
+
+
+class ToggleModule(commands.Cog):
+    def __init__(self, bot: nextcord.Interaction):
+        self.bot = bot
+
+    @nextcord.slash_command(
+        name="pyrio-toggle-module",
+        description="Toggle any module off or on.",
+        force_global=True
+    )
+    async def toggle_module(self, ctx: nextcord.Interaction):
+        pass
+
+
+def setup(bot):
+    bot.add_cog(ToggleModule(bot))
