c68b1d4258ad2923ae2cc4a074919164ff14d2d9
Redtronics <development@redtronics.de>
2023-01-07 13:35:11 +0100

    add toggle command

---
+++ bot/pyrio/commands/toggleCommands/toggleCommand.py
@@ -0,0 +1,19 @@
+import nextcord
+from nextcord.ext import commands
+
+
+class ToggleCommand(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @nextcord.slash_command(
+        name="pyrio-toggle-command",
+        description="Toggle any command on or off.",
+        force_global=True
+    )
+    async def toggle_command(self, ctx: nextcord.Interaction):
+        pass
+
+
+def setup(bot):
+    bot.add_cog(ToggleCommand(bot))
