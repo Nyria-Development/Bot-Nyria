e0160247a3efbcd871e4b758f1999c83ef1247f5
Redtronics <development@redtronics.de>
2023-01-09 18:59:18 +0100

    only logging by errors

--- website/site.py
+++ website/site.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+import logging
 
 
 class WebserverNyria(Flask):
     def __init__(self):
         super().__init__(__name__)
+        self.log = logging.getLogger("werkzeug")
+        self.log.setLevel(logging.ERROR)
 
         @self.route("/")
         def index():
