34622a71495105ed7fb698823dc57362a42055b9
Redtronics <development@redtronics.de>
2023-01-15 18:56:17 +0100

    add resume command

---
+++ bot/diasio/music/resume.py
@@ -0,0 +1,26 @@
+import nextcord.ext
+from nextcord.ext import commands
+import wavelink
+
+
+class MusicResume(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @nextcord.slash_command(
+        name="diasio-music-resume",
+        description="Resume song in voice channel.",
+        guild_ids=[1043477521473212547, 1032632067307085955]
+    )
+    async def resume(self, ctx: nextcord.Interaction):
+        node = wavelink.NodePool.get_node()
+        player = node.get_player(ctx.guild)
+
+        if not player.is_paused():
+            return await ctx.send("The song is playing")
+        await player.resume()
+        await ctx.send("The song is playing again")
+
+
+def setup(bot):
+    bot.add_cog(MusicResume(bot))
