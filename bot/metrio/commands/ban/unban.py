eaf50ff13ac0e73b4e612084defa47862add2243
Redtronics <development@redtronics.de>
2023-01-15 12:07:20 +0100

    add unban command

---
+++ bot/metrio/commands/ban/unban.py
@@ -0,0 +1,19 @@
+import nextcord
+from nextcord.ext import commands
+
+
+class Unban(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @nextcord.slash_command(
+        name="metrio-unban",
+        description="Unban a member",
+        force_global=True
+    )
+    async def unban(self, ctx: nextcord.Interaction):
+        pass
+
+
+def setup(bot):
+    bot.add_cog(Unban(bot))
