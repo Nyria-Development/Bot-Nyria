e81eb69754b89ee682928c45f56a7b6fcac80fb7
Redtronics <development@redtronics.de>
2023-01-18 17:00:39 +0100

    fix logical mistake by delete talk

--- bot/kanio/listeners/voice/talkManager.py
+++ bot/kanio/listeners/voice/talkManager.py
@@ -5,6 +5,7 @@ from nextcord.ext import commands
 class TalkManager(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.perms = ("priority_speaker", "mute_members")
 
     @commands.Cog.listener()
     async def on_voice_state_update(
@@ -20,12 +21,11 @@ class TalkManager(commands.Cog):
                 return await member.send("There are to many voices. Please try again later.")
 
             channel = await member.guild.create_voice_channel(name=f"{member.name}-VC", category=category)
-
             await channel.set_permissions(target=member, priority_speaker=True)
             await member.move_to(channel)
 
-        if str(before.channel) == f"{member.name}-VC":
-            channel = nextcord.utils.get(category.channels, name=f"{member.name}-VC")
+        if str(before.channel) != "Create Voice" and before.channel in category.channels:
+            channel = nextcord.utils.get(category.channels, name=str(before.channel))
 
             if channel.members:
                 return
