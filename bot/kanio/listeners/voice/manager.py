e33fb48787d6714601cca87637a0d88bf2561fac
Redtronics <development@redtronics.de>
2023-02-01 18:36:42 +0100

    fix bugs

--- bot/kanio/listeners/voice/manager.py
+++ bot/kanio/listeners/voice/manager.py
@@ -1,11 +1,11 @@
 import nextcord
 from nextcord.ext import commands
+from src.permissions import voice
 
 
 class VoiceManager(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.perms: dict = {}
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: nextcord.Member, before: nextcord.VoiceState,
@@ -19,10 +19,14 @@ class VoiceManager(commands.Cog):
             channel = await member.guild.create_voice_channel(name=f"{member.name}-VC", category=category)
 
             # setup perms
-            self.perms[f"{channel.id}"] = {"voice_owner": f"{member.name}", "voice_id": f"{channel.id}"}
+            await voice.add_perms(
+                channel_id=channel.id,
+                member=member
+            )
             await member.move_to(channel=channel)
 
         if before.channel in category.channels and str(before.channel) != "Create Voice" and len(before.channel.members) == 0:
+            await voice.remove_perms(before.channel.id)
             channel = self.bot.get_channel(before.channel.id)
             await channel.delete()
 
