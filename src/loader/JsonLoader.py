0ecfc18d794f87bd8d3d0262c8221ade4cd02aaf
Redtronics <development@redtronics.de>
2023-02-03 18:15:24 +0100

    update json loader private

--- src/loader/JsonLoader.py
+++ src/loader/JsonLoader.py
@@ -5,11 +5,12 @@ class JsonLoader:
     def __init__(self):
         with open("config.json", "r") as c:
             self.__config = json.load(c)
-            self.__token = self.__config["token"]
-            self.__host = self.__config["wavelink"]["host"]
-            self.__port = self.__config["wavelink"]["port"]
-            self.__password = self.__config["wavelink"]["password"]
-            self.m_host = self.__config["mariadb"]["host"]
-            self.m_user = self.__config["mariadb"]["user"]
-            self.m_password = self.__config["mariadb"]["password"]
-            self.m_database = self.__config["mariadb"]["database"]
+
+    async def token(self):
+        return self.__config["token"]
+
+    async def wavelink(self):
+        return self.__config["wavelink"]["host"], self.__config["wavelink"]["port"], self.__config["wavelink"]["password"]
+
+    async def maria_db(self):
+        return self.__config["mariadb"]["host"], self.__config["mariadb"]["user"], self.__config["mariadb"]["password"], self.__config["mariadb"]["database"]
