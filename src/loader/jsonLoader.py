787c60d1ec4d951cb2bccc0e05e1f6546a604dd0
Redtronics <development@redtronics.de>
2023-02-19 20:55:24 +0100

    update: remove async functions

--- src/loader/jsonLoader.py
+++ src/loader/jsonLoader.py
@@ -12,7 +12,7 @@ class JsonLoader:
     async def wavelink(self):
         return self.__config["wavelink"]["host"], self.__config["wavelink"]["port"], self.__config["wavelink"]["password"]
 
-    async def maria_db(self):
+    def maria_db(self):
         return self.__config["mariadb"]["host"], self.__config["mariadb"]["user"], self.__config["mariadb"]["password"], self.__config["mariadb"]["database"]
 
     async def get_welcome_colors(self):
