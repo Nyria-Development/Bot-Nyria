ac0e2b60549e28432beb0e7ecf83a0a7f2dc1d52
Redtronics <development@redtronics.de>
2023-03-04 12:24:10 +0100

    feature: add pause command

---
+++ bot/diasio/music/commands/pause.py
@@ -0,0 +1,27 @@
+import nextcord
+from nextcord.ext import commands
+import wavelink
+
+
+class Pause(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @nextcord.slash_command(
+        name="diasio-music-pause",
+        description="Pause a song if bot is playing music.",
+        force_global=True
+    )
+    async def pause(self, ctx: nextcord.Interaction):
+        node = wavelink.NodePool.get_node()
+        player = node.get_player(ctx.guild)
+
+        if not player.is_paused():
+            await player.pause()
+            return await ctx.send("Song is paused")
+
+        return await ctx.send("Song is already paused")
+
+
+def setup(bot):
+    bot.add_cog(Pause(bot))
