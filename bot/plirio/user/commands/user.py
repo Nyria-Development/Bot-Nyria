e84b7fcd22d81eb253aaa424fc65f960469b62af
Redtronics <development@redtronics.de>
2023-03-06 17:49:08 +0100

    feature: add user statistics

---
+++ bot/plirio/user/commands/user.py
@@ -0,0 +1,31 @@
+import nextcord
+from nextcord.ext import commands
+from src.templates import embeds
+
+
+class UserStats(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @nextcord.slash_command(
+        name="plirio-user-stats",
+        description="Show server stats from a user.",
+        force_global=True,
+        default_member_permissions=8
+    )
+    async def user_stats(self, ctx: nextcord.Interaction, user: nextcord.Member):
+        embed_user = embeds.TemplateEmbed(
+            bot=self.bot,
+            ctx=ctx,
+            description="Information | Plirio",
+            color=nextcord.Color.orange()
+        )
+        embed_user.add_field(name="Name", value=user.name)
+        embed_user.add_field(name="Status", value=user.status)
+        embed_user.add_field(name="Roles", value=user.roles)
+
+        await ctx.send(embed=embed_user, ephemeral=True)
+
+
+def setup(bot):
+    bot.add_cog(UserStats(bot))
