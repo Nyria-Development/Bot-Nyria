c0f788de7c42a3ab3c64c7dae8e1dc5785448ed5
meLordlp <antonborch@gmail.com>
2023-03-07 21:54:23 +0900

    Update: Log Update

--- bot/metrio/log/listener/messages.py
+++ bot/metrio/log/listener/messages.py
@@ -9,22 +9,38 @@ class Messages(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message):
         if not message.author.bot:
+            print(message.content)
             log_channel = self.bot.get_channel(1082632691196903424)
-
-            log_embed = nextcord.Embed(
-                title="New message",
-                description=f"A new message from {message.author.mention} was send to {message.channel.mention}.",
-                color=0x081e8c
-            )
+            log_embed = nextcord.Embed(title="New message",
+                                       description=f"A new message from {message.author.mention} was send to {message.channel.mention}.",
+                                       color=0x081e8c)
+            if message.content:
+                log_embed.add_field(name="Content", value=message.content)
             await log_channel.send(embed=log_embed)
 
     @commands.Cog.listener()
-    async def on_message_delete(self, message: str):
-        print(message)
+    async def on_message_delete(self, message):
+        print(message.content)
+        log_channel = self.bot.get_channel(1082632691196903424)
+        log_embed = nextcord.Embed(title="Message was deleted",
+                                   description=f"A message by {message.author.mention} was deleted in {message.channel.mention}.",
+                                   color=0x081e8c)
+        if message.content:
+            log_embed.add_field(name="Content", value=message.content)
+        await log_channel.send(embed=log_embed)
 
     @commands.Cog.listener()
-    async def on_message_edit(self, message: str):
-        print(message)
+    async def on_message_edit(self, befor, after):
+        print(befor.content, after.content)
+        log_channel = self.bot.get_channel(1082632691196903424)
+        log_embed = nextcord.Embed(title="Message was edited",
+                                   description=f"A message from {befor.author.mention} was edited in {befor.channel.mention}.",
+                                   color=0x081e8c)
+        if befor.content:
+            log_embed.add_field(name="befor Content", value=befor.content)
+        if after.content:
+            log_embed.add_field(name="after Content", value=after.content)
+        await log_channel.send(embed=log_embed)
 
 
 def setup(bot):
