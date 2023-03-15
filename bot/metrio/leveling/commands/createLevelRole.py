c68be7ae2bcdd6e1d86e7dad1b3ae447d3955379
meLordlp <antonborch@gmail.com>
2023-03-15 23:13:09 +0900

    update: createLevelRole jetzt mit den standartfarben von discord

--- bot/metrio/leveling/commands/createLevelRole.py
+++ bot/metrio/leveling/commands/createLevelRole.py
@@ -7,6 +7,7 @@ from src.loader.jsonLoader import Leveling, LevelRoles
 class createLevelRole(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.colour_dic = {"white": 0xFFFFFF, "Greyple": 0x99AAb5, "Black": 0x23272A, "DarkButNotBlack": 0x2C2F33, "NotQuiteBlack": 0x23272A, "Blurple": 0x5865F2, "Green": 0x57F287, "Yellow": 0xFEE75C, "Fuchsia": 0xEB459E, "Red": 0xED4245}
 
     @nextcord.slash_command(
         name="metrio-create-level-role",
@@ -14,8 +15,8 @@ class createLevelRole(commands.Cog):
         force_global=True,
         default_member_permissions=8
     )
-    async def create_role(self, ctx: nextcord.Interaction, role_name: str, level: int,):
-        new_level_role = await ctx.guild.create_role(name=role_name, color=0xffffff)
+    async def create_role(self, ctx: nextcord.Interaction, role_name: str, level: int, colour: str = nextcord.SlashOption(choices=list({"white": 0xFFFFFF, "Greyple": 0x99AAb5, "Black": 0x23272A, "DarkButNotBlack": 0x2C2F33, "NotQuiteBlack": 0x23272A, "Blurple": 0x5865F2, "Green": 0x57F287, "Yellow": 0xFEE75C, "Fuchsia": 0xEB459E, "Red": 0xED4245}.keys()))):
+        new_level_role = await ctx.guild.create_role(name=role_name, color=self.colour_dic[colour])
         new_role_embed = nextcord.Embed(title=f"The Level Role, {new_level_role}, was created",
                                         description=f"A New Role from Level {level} was created")
         new_role_embed.add_field(name="Role: ", value=new_level_role.mention)
